558000785b24e6976e579f8afb1a9e6ac40acd07
andrehirano10 <110587452+andrehirano10@users.noreply.github.com>
2023-02-26 21:39:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort, send_file
 from flask_sqlalchemy import SQLAlchemy
 import os
+import matplotlib.pyplot as plt
+from io import BytesIO
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///decisions.db'
@@ -16,56 +18,75 @@ class Decision(db.Model):
     def __repr__(self):
         return '<Decision {}: {} vs {}>'.format(self.id, self.decision1, self.decision2)
 
-
+@app.route('/weigh_decisions', methods=['POST'])
 def weigh_decisions():
-    decision1 = request.json['decision1']
-    decision2 = request.json['decision2']
-    decision1_rating = request.json['decision1_rating']
-    decision2_rating = request.json['decision2_rating']
+    try:
+        decision1 = request.json['decision1']
+        decision2 = request.json['decision2']
+        decision1_rating = request.json['decision1_rating']
+        decision2_rating = request.json['decision2_rating']
 
-    self_efficacy = input("Which decision will make you feel more confident and capable? ({} or {}): enter 1 or 2)  ".format(decision1, decision2)).lower()
-    if self_efficacy == "1":
-        decision1_rating += 1
-    else:
-        decision2_rating += 1
-    growth = input("Which decision will offer you more opportunities to grow and develop as a person? ({} or {}): ".format(decision1, decision2)).lower()
-    if growth == "1":
-        decision1_rating += 1
-    else:
-        decision2_rating += 1
+        self_efficacy = input("Which decision will make you feel more confident and capable? ({} or {}): enter 1 or 2)  ".format(decision1, decision2)).lower()
+        if self_efficacy == "1":
+            decision1_rating += 1
+        else:
+            decision2_rating += 1
+        growth = input("Which decision will offer you more opportunities to grow and develop as a person? ({} or {}): ".format(decision1, decision2)).lower()
+        if growth == "1":
+            decision1_rating += 1
+        else:
+            decision2_rating += 1
 
-    meaning = input("Which decision will give you greater meaning in life? (choose 1 or 2")
-    if meaning == "1":
-        decision1_rating += 1
-    else:
-        decision2_rating += 1
-    regret = input("Are you more likely to regret not make which decision {} or {}? (choose 1 or 2): ".format(decision1, decision2,)).lower()
-    if regret == "1":
-        decision1_rating += 1
-    else:
-        decision2_rating += 1
-    wise_counsel = input("What would the wisest version of yourself counsel you to do? ({} or {}): ".format(decision1, decision2)).lower()
-    if wise_counsel == "1":
-        decision1_rating += 1
-    else:
-        decision2_rating += 1
+        meaning = input("Which decision will give you greater meaning in life? (choose 1 or 2")
+        if meaning == "1":
+            decision1_rating += 1
+        else:
+            decision2_rating += 1
+        regret = input("Are you more likely to regret not make which decision {} or {}? (choose 1 or 2): ".format(decision1, decision2,)).lower()
+        if regret == "1":
+            decision1_rating += 1
+        else:
+            decision2_rating += 1
+        wise_counsel = input("What would the wisest version of yourself counsel you to do? ({} or {}): ".format(decision1, decision2)).lower()
+        if wise_counsel == "1":
+            decision1_rating += 1
+        else:
+            decision2_rating += 1
 
-    decision = Decision(decision1=decision1, decision2=decision2, decision1_rating=decision1_rating, decision2_rating=decision2_rating)
+        decision = Decision(decision1=decision1, decision2=decision2, decision1_rating=decision1_rating, decision2_rating=decision2_rating)
 
-    db.session.add(decision)
-    db.session.commit()
+        db.session.add(decision)
+        db.session.commit()
 
-    if decision1_rating > decision2_rating:
-        print("Go ahead with {}: ".format(decision1))
-    else:
-        print("Go ahead with {}: ".format(decision2))
+        if decision1_rating > decision2_rating:
+            print("Go ahead with {}: ".format(decision1))
+        else:
+            print("Go ahead with {}: ".format(decision2))
 
-    decisions = Decision.query.all()
-    decision_names = [d.decision1 + " vs " + d.decision2 for d in decisions]
-    decision_ratings = [d.decision1_rating + d.decision2_rating for d in decisions]
+        decisions = Decision.query.all()
+        decision_names = [d.decision1 + " vs " + d.decision2 for d in decisions]
+        decision_ratings = [d.decision1_rating + d.decision2_rating for d in decisions]
 
-    plt.bar(decision_names, decision_ratings)
-    plt.show()
+        fig, ax = plt.subplots()
+        ax.bar(decision_names, decision_ratings)
+        ax.set_ylabel('Total rating')
+        ax.set_title('Ratings of all decisions')
 
-if __name__ == '__main__':
-    app.run()
+        # Save graph to in-memory buffer
+        buffer = BytesIO()
+        fig.savefig(buffer, format='png')
+        buffer.seek(0)
+
+        # Return image as response to request
+        return send_file (buffer, mimetype='image/png')
+        
+    except KeyError as e:
+        abort(400, description=f"Missing field: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    abort(500, description="An error occurred while processing the request")
+    
+    if __name__ == '__main__':       
+        app.run()
+
+
